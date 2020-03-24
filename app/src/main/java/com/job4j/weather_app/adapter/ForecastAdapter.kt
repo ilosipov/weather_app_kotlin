447c7047d0e61fbfa5f61b70d3d58dd9f32fe1db
Ilya Osipov <il.osipov.ya@yandex.ru>
2020-03-24 12:06:39 +0300

    list of weather forecasts updated, additional information added. weather app

--- app/src/main/java/com/job4j/weather_app/adapter/ForecastAdapter.kt
+++ app/src/main/java/com/job4j/weather_app/adapter/ForecastAdapter.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 import com.airbnb.lottie.LottieAnimationView
 import com.job4j.weather_app.R
 import com.job4j.weather_app.model.Day
@@ -31,6 +35,7 @@ class ForecastAdapter(private var context: Context, private var resource: Int,
 
     override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
         val day = days[position]
+        val itemView = holder.itemView
 
         val date = holder.itemView.findViewById<TextView>(R.id.forecast_date)
         date.text = setDateToConvert(day.dt_txt)
@@ -43,11 +48,32 @@ class ForecastAdapter(private var context: Context, private var resource: Int,
 
         val temp = holder.itemView.findViewById<TextView>(R.id.forecast_temp)
         temp.text = String.format("%s°", day.main!!.temp.toInt())
+
+        val wind = holder.itemView.findViewById<TextView>(R.id.forecast_wind)
+        wind.text = String.format("%skm/h", day.wind!!.speed.toInt())
+
+        val humidity = holder.itemView.findViewById<TextView>(R.id.forecast_humidity)
+        humidity.text = String.format("%s%%", day.main!!.humidity.toInt())
+
+        val pressure = holder.itemView.findViewById<TextView>(R.id.forecast_pressure)
+        pressure.text = String.format("%smm", day.main!!.pressure.toInt())
+
+        itemView.setOnClickListener {
+            val forecastCard = holder.itemView.findViewById<CardView>(R.id.forecast_card_view)
+            val detailsLayout = holder.itemView.findViewById<ConstraintLayout>(R.id.forecast_detail_layout)
+            if (detailsLayout.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(forecastCard, AutoTransition())
+                detailsLayout.visibility = View.VISIBLE
+            } else {
+                detailsLayout.visibility = View.GONE
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return days.size
     }
+
 
     @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     private fun setDateToConvert(date: String) : String {
--- app/src/main/java/com/job4j/weather_app/fragment/ForecastBottomFragment.kt
+++ app/src/main/java/com/job4j/weather_app/fragment/ForecastBottomFragment.kt
@@ -43,7 +43,8 @@ class ForecastBottomFragment : BottomSheetDialogFragment() {
         return fragment
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_bottom_forecast, container, false)
 
         forecastRecycler = view.findViewById(R.id.recycler_forecast)
@@ -55,6 +56,9 @@ class ForecastBottomFragment : BottomSheetDialogFragment() {
 
         return view
     }
+
+    private val forecastWeatherResponse : MutableLiveData<ForecastWeather>
+        get() = callbackForecastWeather
 
     private fun updateUI(lat: String, lon: String) {
         requestWeather.getForecastWeather(lat, lon, callbackForecastWeather)
@@ -71,7 +75,4 @@ class ForecastBottomFragment : BottomSheetDialogFragment() {
             progressForecast.visibility = View.GONE
         })
     }
-
-    private val forecastWeatherResponse : MutableLiveData<ForecastWeather>
-        get() = callbackForecastWeather
 }
